b72cbb44ec0e4a40b4e529d2ba2ae2b0f73a48ab
Cyril Volle <cyril.volle@inetum.com>
2024-07-01 16:55:44 +0200

    Try and catch to return error XML importer

--- src/Service/XMLImporter.php
+++ src/Service/XMLImporter.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Service;
 
@@ -6,10 +6,28 @@ use SimpleXMLElement;
 
 class XMLImporter
 {
-    public function XMLImporterFeed($url): SimpleXMLElement|false
+    /**
+     * This PHP function loads an XML feed from a specified URL and returns the channel element as a
+     * SimpleXMLElement object or false if the feed cannot be loaded.
+     * 
+     * @param url The `url` parameter in the `xmlImporterFeed` function is expected to be a string
+     * representing the URL of an XML feed that you want to import. This function uses
+     * `simplexml_load_file` to load the XML file from the provided URL and then returns the `channel`
+     * element of the
+     * 
+     * @return SimpleXMLElement|false The function `xmlImporterFeed` is returning a `SimpleXMLElement`
+     * object if the XML file is successfully loaded from the provided URL. If there is an issue
+     * loading the file, it will return `false`.
+     */
+    public function xmlImporterFeed($url): SimpleXMLElement|array
     {
-        $feed = simplexml_load_file($url);
-        if($feed) {
+        $feed = false;
+        try {
+            $feed = simplexml_load_file($url);
+        } catch (\Throwable $th) {
+            return ['type' => 'error', 'message' => $th];
+        }
+        if ($feed) {
             $feed = $feed->channel;
         }
         return $feed;
